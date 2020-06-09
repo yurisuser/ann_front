bf34814dbe4f8b0a8fd00c62f63cb1d8981f0dc4
yurisuser <yurisuser@github.com>
2020-06-09 12:49:25 +0300

    galery_template

--- src/app/components/galery/galery.component.ts
+++ src/app/components/galery/galery.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap, map } from 'rxjs/operators';
+
+import { IGaleryElement } from '../../models/galeryElements';
+import { DataService } from '../../services/data.service';
 
 @Component({
-  selector: 'app-galery',
-  templateUrl: './galery.component.html',
-  styleUrls: ['./galery.component.scss']
+    selector: 'app-galery',
+    templateUrl: './galery.component.html',
+    styleUrls: ['./galery.component.scss']
 })
 export class GaleryComponent implements OnInit {
 
-  constructor() { }
+    galeryShowArr: IGaleryElement[];
 
-  ngOnInit() {
-  }
+    constructor(
+        private dataSrv: DataService,
+        private route: ActivatedRoute,
+    ) { }
+
+    ngOnInit() {
+        this.route.params.pipe(
+            switchMap(x => this.dataSrv.getGalleryElements().pipe(
+                    map(z  =>  !x.type ? z : z.filter(c => c.galeryType === Number(x.type)))))
+            )
+        .subscribe(x => {
+            this.galeryShowArr = x;
+        });
+    }
+
+    getFullImgPath(img: string) {
+        return this.dataSrv.getFullImgPath(img);
+    }
 
 }
